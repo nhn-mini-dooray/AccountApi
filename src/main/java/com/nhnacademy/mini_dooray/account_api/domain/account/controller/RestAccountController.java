363823a39137f81e4b760b52a5c1f3c70f1c8dd8
김덕준 <710dkjn@gmail.com>
2023-06-05 20:33:29 +0900

    feat: 🎸 추가 RestAccountController 생성

---
+++ src/main/java/com/nhnacademy/mini_dooray/account_api/domain/account/controller/RestAccountController.java
@@ -0,0 +1,57 @@
+package com.nhnacademy.mini_dooray.account_api.domain.account.controller;
+
+import com.nhnacademy.mini_dooray.account_api.domain.account.model.request.CreateAccountRequestDto;
+import com.nhnacademy.mini_dooray.account_api.domain.account.service.AccountService;
+import com.nhnacademy.mini_dooray.account_api.domain.status.model.request.StatusCodeRequestDto;
+import com.nhnacademy.mini_dooray.account_api.exception.ValidationFailedException;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@RequestMapping("/accounts")
+public class RestAccountController {
+    private final AccountService accountService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signupHandler(@Valid @RequestBody CreateAccountRequestDto responseDto,
+                                              BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            throw new ValidationFailedException(bindingResult);
+        }
+
+        accountService.createAccount(responseDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .build();
+    }
+
+    @PutMapping("/{id}/dormant")
+    public ResponseEntity<Void> dormantHandler(@PathVariable(name = "id") Long id,
+                                               @RequestBody StatusCodeRequestDto status,
+                                               BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new ValidationFailedException(bindingResult);
+        }
+
+        accountService.updateAccount(id, status);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @DeleteMapping("/{id}/withdrawal")
+    public ResponseEntity<Void> withdrawalHandler(@PathVariable(name = "id") Long id) {
+        accountService.deleteAccount(id);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .build();
+    }
+
+}

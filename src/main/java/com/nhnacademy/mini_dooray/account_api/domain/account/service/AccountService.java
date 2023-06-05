5157d9cbc86c2e461ba1b4ee19866be46d5ad41a
김덕준 <710dkjn@gmail.com>
2023-06-05 20:32:03 +0900

    feat: 🎸 추가 Account Service 생성

---
+++ src/main/java/com/nhnacademy/mini_dooray/account_api/domain/account/service/AccountService.java
@@ -0,0 +1,58 @@
+package com.nhnacademy.mini_dooray.account_api.domain.account.service;
+
+import com.nhnacademy.mini_dooray.account_api.domain.account.entity.Account;
+import com.nhnacademy.mini_dooray.account_api.domain.account.model.request.CreateAccountRequestDto;
+import com.nhnacademy.mini_dooray.account_api.domain.account.repository.AccountRepository;
+import com.nhnacademy.mini_dooray.account_api.domain.status.entity.Status;
+import com.nhnacademy.mini_dooray.account_api.domain.status.model.request.StatusCodeRequestDto;
+import com.nhnacademy.mini_dooray.account_api.domain.status.model.type_code.StatusCode;
+import com.nhnacademy.mini_dooray.account_api.domain.status.repository.StatusRepository;
+import com.nhnacademy.mini_dooray.account_api.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class AccountService {
+    private final AccountRepository accountRepository;
+    private final StatusRepository statusRepository;
+
+    @Transactional(readOnly = true)
+    public List<Account> getAccounts() {
+        return accountRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Account getAccount(Long id) {
+        return accountRepository.findById(id).orElseThrow(NotFoundException::new);
+    }
+
+    public void createAccount(CreateAccountRequestDto requestDto) {
+        Status status = statusRepository.findByName(StatusCode.ACTIVE).orElseThrow(NotFoundException::new);
+
+        Account account = Account.builder()
+                .status(status)
+                .loginId(requestDto.getLoginId())
+                .email(requestDto.getEmail())
+                .password(requestDto.getPassword())
+                .build();
+        accountRepository.save(account);
+    }
+
+    public void updateAccount(Long id, StatusCodeRequestDto statusCode) { //statusCode가 잘 작동되는지 확인
+        Status status = statusRepository.findByName(statusCode.getStatus()).orElseThrow(NotFoundException::new);
+        Account account = accountRepository.findById(id).orElseThrow(NotFoundException::new);
+        account.setStatus(status);
+        accountRepository.save(account);
+    }
+
+    public void deleteAccount(Long id) {
+        accountRepository.deleteById(id);
+    }
+
+
+}

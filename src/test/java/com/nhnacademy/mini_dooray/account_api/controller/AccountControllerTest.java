b400d971802ce2c891e0a9670831ffceb249b0d4
김덕준 <710dkjn@gmail.com>
2023-06-06 21:37:44 +0900

    test: 💍 추가 AccountControllerTest emailTest 테스트 코드 작성

--- src/test/java/com/nhnacademy/mini_dooray/account_api/controller/AccountControllerTest.java
+++ src/test/java/com/nhnacademy/mini_dooray/account_api/controller/AccountControllerTest.java
@@ -3,8 +3,12 @@ package com.nhnacademy.mini_dooray.account_api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.mini_dooray.account_api.domain.account.controller.RestAccountController;
+import com.nhnacademy.mini_dooray.account_api.domain.account.entity.Account;
 import com.nhnacademy.mini_dooray.account_api.domain.account.model.request.CreateAccountRequestDto;
+import com.nhnacademy.mini_dooray.account_api.domain.account.model.request.EmailRequestDto;
+import com.nhnacademy.mini_dooray.account_api.domain.account.model.response.FindByEmailResponseDto;
 import com.nhnacademy.mini_dooray.account_api.domain.account.service.AccountService;
+import com.nhnacademy.mini_dooray.account_api.domain.status.entity.Status;
 import com.nhnacademy.mini_dooray.account_api.domain.status.model.request.StatusCodeRequestDto;
 import com.nhnacademy.mini_dooray.account_api.domain.status.model.type_code.StatusCode;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,8 +20,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(RestAccountController.class)
@@ -76,5 +87,61 @@ public class AccountControllerTest {
         mvc.perform(delete("/accounts/" + accountId.intValue() + "/withdrawal"))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    @DisplayName("email 테스트")
+    void emailTest() throws Exception {
+        EmailRequestDto emailRequestDto = new EmailRequestDto("jane@example.com");
+
+        FindByEmailResponseDto findByEmailResponseDto = new FindByEmailResponseDto() {
+            @Override
+            public Long getAccountId() {
+                return 2L;
+            }
+
+            @Override
+            public Status getStatus() {
+                return new Status(1, StatusCode.ACTIVE);
+            }
+
+            @Override
+            public String getLoginId() {
+                return "jane";
+            }
+
+            @Override
+            public String getPassword() {
+                return "password456";
+            }
+
+            @Override
+            public LocalDate getLastLoginDate() {
+                return LocalDate.parse("2023-06-06");
+            }
+        };
+
+        //given(accountService.findAccountByEmail(emailRequestDto)).willReturn(
+        given(accountService.findAccountByEmail(any(EmailRequestDto.class))).willReturn(findByEmailResponseDto);
+
+        mvc.perform(post("/accounts/email")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(emailRequestDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accountId", equalTo(2)))
+                .andExpect(jsonPath("$.password", equalTo("password456")))
+                .andExpect(jsonPath("$.lastLoginDate", equalTo("2023-06-06")))
+                .andExpect(jsonPath("$.status.statusId", equalTo(1)))
+                .andExpect(jsonPath("$.status.name", equalTo("ACTIVE")))
+                .andExpect(jsonPath("$.loginId", equalTo("jane")));
+
+
+
+
+
+
+
+
+    }
+
 
 }

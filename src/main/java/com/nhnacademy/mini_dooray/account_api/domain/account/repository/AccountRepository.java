c4ecac6a8c81078c8a85e3d02bc12c6bad7709ab
김덕준 <710dkjn@gmail.com>
2023-06-06 17:53:22 +0900

    feat: 🎸 추가 Repository findByEmail 메서드 생성

--- src/main/java/com/nhnacademy/mini_dooray/account_api/domain/account/repository/AccountRepository.java
+++ src/main/java/com/nhnacademy/mini_dooray/account_api/domain/account/repository/AccountRepository.java
@@ -1,9 +1,10 @@
 package com.nhnacademy.mini_dooray.account_api.domain.account.repository;
 
 import com.nhnacademy.mini_dooray.account_api.domain.account.entity.Account;
+import com.nhnacademy.mini_dooray.account_api.domain.account.model.response.FindByEmailResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-
+    FindByEmailResponseDto findByEmail(String email);
 }

a402320140cea7a3605dc6b06e68ec5a7b298d53
eora21 <eora21@naver.com>
2023-06-13 16:20:41 +0900

    feat: 🎸 추가 로그인 아이디와 이메일에 대해 존재 여부 및 로그인 처리에 관한 메서드 작성

--- src/main/java/com/nhnacademy/mini_dooray/account_api/domain/account/repository/AccountRepository.java
+++ src/main/java/com/nhnacademy/mini_dooray/account_api/domain/account/repository/AccountRepository.java
@@ -1,10 +1,16 @@
 package com.nhnacademy.mini_dooray.account_api.domain.account.repository;
 
 import com.nhnacademy.mini_dooray.account_api.domain.account.entity.Account;
-import com.nhnacademy.mini_dooray.account_api.domain.account.model.response.FindByEmailResponseDto;
+import com.nhnacademy.mini_dooray.account_api.domain.account.model.response.AccountLoginDto;
+import com.nhnacademy.mini_dooray.account_api.domain.account.model.response.EmailResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    FindByEmailResponseDto findByEmail(String email);
+    Optional<EmailResponseDto> findByEmail(String email);
+    Boolean existsByLoginId(String loginId);
+    Boolean existsByEmail(String email);
+    Optional<AccountLoginDto> findLoginDtoByLoginId(String loginId);
 }

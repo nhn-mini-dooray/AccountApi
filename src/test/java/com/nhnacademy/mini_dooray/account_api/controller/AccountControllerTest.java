a2bd943ddcb168b08adbe90c137c2711440100e0
eora21 <eora21@naver.com>
2023-06-13 16:50:39 +0900

    refactor: 💡 변경 메서드 및 dto 수정으로 인한 테스트 수정

--- src/test/java/com/nhnacademy/mini_dooray/account_api/controller/AccountControllerTest.java
+++ src/test/java/com/nhnacademy/mini_dooray/account_api/controller/AccountControllerTest.java
@@ -5,9 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.mini_dooray.account_api.domain.account.controller.RestAccountController;
 import com.nhnacademy.mini_dooray.account_api.domain.account.model.request.CreateAccountRequestDto;
 import com.nhnacademy.mini_dooray.account_api.domain.account.model.request.EmailRequestDto;
-import com.nhnacademy.mini_dooray.account_api.domain.account.model.response.FindByEmailResponseDto;
+import com.nhnacademy.mini_dooray.account_api.domain.account.model.response.EmailResponseDto;
 import com.nhnacademy.mini_dooray.account_api.domain.account.service.AccountService;
-import com.nhnacademy.mini_dooray.account_api.domain.status.entity.Status;
 import com.nhnacademy.mini_dooray.account_api.domain.status.model.request.StatusCodeRequestDto;
 import com.nhnacademy.mini_dooray.account_api.domain.status.model.type_code.StatusCode;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,8 +17,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.time.LocalDate;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
@@ -91,44 +88,25 @@ class AccountControllerTest {
     void emailTest() throws Exception {
         EmailRequestDto emailRequestDto = new EmailRequestDto("jane@example.com");
 
-        FindByEmailResponseDto findByEmailResponseDto = new FindByEmailResponseDto() {
+        EmailResponseDto emailResponseDto = new EmailResponseDto() {
             @Override
             public Long getAccountId() {
                 return 2L;
             }
 
             @Override
-            public Status getStatus() {
-                return new Status(1, StatusCode.ACTIVE);
-            }
-
-            @Override
             public String getLoginId() {
                 return "jane";
             }
-
-            @Override
-            public String getPassword() {
-                return "password456";
-            }
-
-            @Override
-            public LocalDate getLastLoginDate() {
-                return LocalDate.parse("2023-06-06");
-            }
         };
 
-        given(accountService.findAccountByEmail(any(EmailRequestDto.class))).willReturn(findByEmailResponseDto);
+        given(accountService.findAccountByEmail(any(EmailRequestDto.class))).willReturn(emailResponseDto);
 
         mvc.perform(post("/accounts/email")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(emailRequestDto)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.accountId", equalTo(2)))
-                .andExpect(jsonPath("$.password", equalTo("password456")))
-                .andExpect(jsonPath("$.lastLoginDate", equalTo("2023-06-06")))
-                .andExpect(jsonPath("$.status.statusId", equalTo(1)))
-                .andExpect(jsonPath("$.status.name", equalTo("ACTIVE")))
                 .andExpect(jsonPath("$.loginId", equalTo("jane")));
     }
 
